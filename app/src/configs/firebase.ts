f3760c3d8ace959ff44e90196dbdf7d96c4faa27
Fredrik Johansen <fredrik1206@gmail.com>
2025-06-21 10:38:57 +0200

    feat(app): adds firebase config file

---
+++ app/src/configs/firebase.ts
@@ -0,0 +1,11 @@
+import type { FirebaseOptions } from "firebase/app";
+
+export const firebaseConfig: FirebaseOptions = {
+  apiKey: "AIzaSyDFMm1-zwxexPIeU1_UR-6ROnG_3bKPVrU",
+  authDomain: "pin-scout.firebaseapp.com",
+  projectId: "pin-scout",
+  storageBucket: "pin-scout.firebasestorage.app",
+  messagingSenderId: "759133125883",
+  appId: "1:759133125883:web:e2e414aeb328801c59845e",
+  measurementId: "G-5MHMQWCDSC",
+};
