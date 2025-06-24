cc7e2b0ea0920c59510e36ddc8ae419d1298ff93
Fredrik Johansen <fredrik1206@gmail.com>
2025-06-24 19:30:12 +0200

    feat(app): adds tanstackRouter plugin to vite config file

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -2,13 +2,21 @@ import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   build: {
     outDir: "build",
   },
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true,
+    }),
+    tailwindcss(),
+    react(),
+  ],
   resolve: {
     alias: {
       "@shadcn-ui": path.resolve(__dirname, "./src/shadcn-ui"),
