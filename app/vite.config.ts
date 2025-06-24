659ba5682785ee80912ac1383ef3df341006e63a
Fredrik Johansen <fredrik1206@gmail.com>
2025-06-24 20:21:23 +0200

    feat(app): adds .env file dir to vite config

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -4,8 +4,8 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
+  envDir: "../",
   build: {
     outDir: "build",
   },
