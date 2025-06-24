d94905eb83412561a310044115f39dffea7d0ef9
Fredrik Johansen <fredrik1206@gmail.com>
2025-06-24 06:35:13 +0200

    feat(app): adds tailwindcss plugin to vite config file

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   build: {
     outDir: "build",
   },
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
 });
