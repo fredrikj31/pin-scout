8a7953306e4b21b0482165bf3fccc89b24235f51
Fredrik Johansen <fredrik1206@gmail.com>
2025-06-24 06:35:29 +0200

    feat(app): adds tailwind css import to index.css file

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -4,7 +4,7 @@ import { FirebaseProvider } from "./providers/firebase/Provider";
 export const App = () => {
   return (
     <FirebaseProvider>
-      <h1>Hello World</h1>
+      <h1 className="text-xl">Hello World</h1>
       <EventButton />
     </FirebaseProvider>
   );
