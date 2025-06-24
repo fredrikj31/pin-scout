1def40c43717370648a0acec93bb6aa6a0301552
Fredrik Johansen <fredrik1206@gmail.com>
2025-06-24 19:45:14 +0200

    feat(app): adds ReactQueryProvider to root route

--- app/src/routes/__root.tsx
+++ app/src/routes/__root.tsx
@@ -1,14 +1,19 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { FirebaseProvider } from "../providers/firebase/Provider";
 import "../index.css";
+
+const queryClient = new QueryClient();
 
 export const Route = createRootRoute({
   component: () => (
     <>
-      <FirebaseProvider>
-        <Outlet />
-      </FirebaseProvider>
+      <QueryClientProvider client={queryClient}>
+        <FirebaseProvider>
+          <Outlet />
+        </FirebaseProvider>
+      </QueryClientProvider>
       <TanStackRouterDevtools />
     </>
   ),
