d9ab691c5523526302b4c3287b086e567ef5553a
Fredrik Johansen <fredrik1206@gmail.com>
2025-06-24 20:44:42 +0200

    feat(routes): adds signup route and layout

--- app/src/routes/signup.tsx
+++ app/src/routes/signup.tsx
@@ -1,9 +1,92 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { Button } from "@shadcn-ui/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@shadcn-ui/components/ui/card";
+import { Input } from "@shadcn-ui/components/ui/input";
+import { Label } from "@shadcn-ui/components/ui/label";
+import { createFileRoute, Link } from "@tanstack/react-router";
+import { useCreateUser } from "../api/createUser/useCreateUser";
+import { useRef } from "react";
 
 export const Route = createFileRoute("/signup")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <div>Hello "/signup"!</div>;
+  const emailInput = useRef<HTMLInputElement>(null);
+  const passwordInput = useRef<HTMLInputElement>(null);
+
+  const { mutate: createUser } = useCreateUser();
+
+  const signup = () => {
+    const email = emailInput.current?.value;
+    const password = passwordInput.current?.value;
+
+    if (!email || !password) return;
+
+    createUser(
+      { email, password },
+      {
+        onError: () => {
+          alert("Failed to sign up!");
+        },
+        onSuccess: () => {
+          alert("Signed up!");
+        },
+      }
+    );
+  };
+
+  return (
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <div className="flex flex-col gap-6">
+          <Card>
+            <CardHeader>
+              <CardTitle>Create your user</CardTitle>
+              <CardDescription>Sign up and create a profile</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <div className="flex flex-col gap-6">
+                <div className="grid gap-3">
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                    ref={emailInput}
+                    id="email"
+                    type="email"
+                    placeholder="johndoe@example.com"
+                    required
+                  />
+                </div>
+                <div className="grid gap-3">
+                  <Label htmlFor="password">Password</Label>
+                  <Input
+                    ref={passwordInput}
+                    id="password"
+                    type="password"
+                    required
+                  />
+                </div>
+                <div className="flex flex-col gap-3">
+                  <Button onClick={() => signup()} className="w-full">
+                    Signup
+                  </Button>
+                </div>
+              </div>
+              <div className="mt-4 text-center text-sm">
+                Already have an account?{" "}
+                <Link to="/" className="underline underline-offset-4">
+                  Login
+                </Link>
+              </div>
+            </CardContent>
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
 }
