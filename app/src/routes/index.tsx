5f9d921a6f6ac59519f716bca6ff208cbe7e7653
Fredrik Johansen <fredrik1206@gmail.com>
2025-06-25 20:02:08 +0200

    feat(app): adds user email to index route if user is logged in

--- app/src/routes/index.tsx
+++ app/src/routes/index.tsx
@@ -1,14 +1,17 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { EventButton } from "../components/EventButton";
+import { useAuth } from "../providers/auth/useAuth";
 
 export const Route = createFileRoute("/")({
   component: Index,
 });
 
 function Index() {
+  const auth = useAuth();
+
   return (
     <div className="p-2">
-      <h1 className="text-xl">Hello World</h1>
+      <h1 className="text-xl">Hello {auth.user?.email}</h1>
       <EventButton />
     </div>
   );
