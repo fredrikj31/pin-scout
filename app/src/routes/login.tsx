d5baa968eeb5a45fc03e2c2f11368e09e399b95e
Fredrik Johansen <fredrik1206@gmail.com>
2025-06-25 19:39:07 +0200

    feat(app): adds login route and layout

---
+++ app/src/routes/login.tsx
@@ -0,0 +1,94 @@
+import { Label } from "@radix-ui/react-label";
+import { Button } from "@shadcn-ui/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@shadcn-ui/components/ui/card";
+import { Input } from "@shadcn-ui/components/ui/input";
+import { createFileRoute, Link } from "@tanstack/react-router";
+import { useRef } from "react";
+import { useLoginUser } from "../api/loginUser/useLoginUser";
+
+export const Route = createFileRoute("/login")({
+  component: RouteComponent,
+});
+
+function RouteComponent() {
+  const emailInput = useRef<HTMLInputElement>(null);
+  const passwordInput = useRef<HTMLInputElement>(null);
+
+  const { mutate: loginUser } = useLoginUser();
+
+  const login = () => {
+    const email = emailInput.current?.value;
+    const password = passwordInput.current?.value;
+
+    if (!email || !password) return;
+
+    loginUser(
+      { email, password },
+      {
+        onError: () => {
+          alert("Failed to login!");
+        },
+        onSuccess: () => {
+          alert("Logged in!");
+        },
+      }
+    );
+  };
+
+  return (
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <div className="flex flex-col gap-6">
+          <Card>
+            <CardHeader>
+              <CardTitle>Login to your account</CardTitle>
+              <CardDescription>
+                Enter your email below to login to your account
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <div className="flex flex-col gap-6">
+                <div className="grid gap-3">
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                    ref={emailInput}
+                    id="email"
+                    type="email"
+                    placeholder="johndoe@example.com"
+                    required
+                  />
+                </div>
+                <div className="grid gap-3">
+                  <Label htmlFor="password">Password</Label>
+                  <Input
+                    ref={passwordInput}
+                    id="password"
+                    type="password"
+                    required
+                  />
+                </div>
+                <div className="flex flex-col gap-3">
+                  <Button onClick={() => login()} className="w-full">
+                    Login
+                  </Button>
+                </div>
+              </div>
+              <div className="mt-4 text-center text-sm">
+                Already have an account?{" "}
+                <Link to="/signup" className="underline underline-offset-4">
+                  Signup
+                </Link>
+              </div>
+            </CardContent>
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
+}
