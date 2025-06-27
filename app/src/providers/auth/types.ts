21baa9d9b1d1ab949f6cbe42d4296be47140d715
Fredrik Johansen <fredrik1206@gmail.com>
2025-06-27 21:47:10 +0200

    feat(app): adds logout function to auth provider

--- app/src/providers/auth/types.ts
+++ app/src/providers/auth/types.ts
@@ -4,4 +4,5 @@ export interface AuthProviderValue {
   user: User | null | undefined; // User = logged in || null = logged out || undefined = loading
   login: ({ email, password }: { email: string; password: string }) => void;
   signup: ({ email, password }: { email: string; password: string }) => void;
+  logout: () => void;
 }
