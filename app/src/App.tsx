8c57a9c6e820a489c7e4c1759389708610dcf3d0
Fredrik Johansen <fredrik1206@gmail.com>
2025-06-27 21:20:10 +0200

    feat(app): adds App.tsx file to main.tsx file

---
+++ app/src/App.tsx
@@ -0,0 +1,28 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router";
+import { FirebaseProvider } from "./providers/firebase/Provider";
+import { AuthProvider } from "./providers/auth/Provider";
+import { HomePage } from "./pages/home";
+import "./index.css";
+import { LoginPage } from "./pages/login";
+import { SignupPage } from "./pages/signup";
+
+export const App = () => {
+  const queryClient = new QueryClient();
+
+  return (
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <FirebaseProvider>
+          <AuthProvider>
+            <Routes>
+              <Route path="/" index element={<HomePage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/signup" element={<SignupPage />} />
+            </Routes>
+          </AuthProvider>
+        </FirebaseProvider>
+      </QueryClientProvider>
+    </BrowserRouter>
+  );
+};
