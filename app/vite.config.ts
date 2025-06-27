70e42cea80cb597476df1f68e7a6bcd512b5e834
Fredrik Johansen <fredrik1206@gmail.com>
2025-06-27 21:18:44 +0200

    chore(app): removes @tanstack/router plugin from vite config

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -2,21 +2,13 @@ import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { tanstackRouter } from "@tanstack/router-plugin/vite";
 
 export default defineConfig({
   envDir: "../",
   build: {
     outDir: "build",
   },
-  plugins: [
-    tanstackRouter({
-      target: "react",
-      autoCodeSplitting: true,
-    }),
-    tailwindcss(),
-    react(),
-  ],
+  plugins: [tailwindcss(), react()],
   resolve: {
     alias: {
       "@shadcn-ui": path.resolve(__dirname, "./src/shadcn-ui"),
