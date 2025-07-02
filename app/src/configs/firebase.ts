602b8a9705ebcf4bd227a22751d8fd63e1cccdc1
Fredrik Johansen <fredrik1206@gmail.com>
2025-07-02 19:27:31 +0200

    feat(app): adds rest of firebase config properties to env variables

--- app/src/configs/firebase.ts
+++ app/src/configs/firebase.ts
@@ -3,10 +3,10 @@ import { config } from "../config";
 
 export const firebaseConfig: FirebaseOptions = {
   apiKey: config.firebase.apiKey,
-  authDomain: "pin-scout.firebaseapp.com",
-  projectId: "pin-scout",
-  storageBucket: "pin-scout.firebasestorage.app",
-  messagingSenderId: "759133125883",
-  appId: "1:759133125883:web:e2e414aeb328801c59845e",
-  measurementId: "G-5MHMQWCDSC",
+  authDomain: config.firebase.authDomain,
+  projectId: config.firebase.projectId,
+  storageBucket: config.firebase.storageBucket,
+  messagingSenderId: config.firebase.messagingSenderId,
+  appId: config.firebase.appId,
+  measurementId: config.firebase.measurementId,
 };
