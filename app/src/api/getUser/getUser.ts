80ac268a2ad23f196b633a324c20220c6e0d15aa
Fredrik Johansen <fredrik1206@gmail.com>
2025-07-03 20:20:02 +0200

    feat(app): adds getUser api call and react query hook

---
+++ app/src/api/getUser/getUser.ts
@@ -0,0 +1,23 @@
+import { doc, getDoc, type Firestore } from "firebase/firestore";
+import { z } from "zod";
+
+interface GetUserOptions {
+  firestore: Firestore;
+  userId: string;
+}
+export const getUser = async ({
+  firestore,
+  userId,
+}: GetUserOptions): Promise<{ firstName: string }> => {
+  // Get Firestore user document
+  const document = await getDoc(doc(firestore, "users", userId)).catch(
+    (error: unknown) => {
+      console.log("Failed to create user profile document in Firestore", error);
+      throw error;
+    }
+  );
+
+  const user = z.object({ firstName: z.string() }).parse(document.data());
+
+  return user;
+};
---
+++ app/src/api/getUser/useGetUser.ts
@@ -0,0 +1,18 @@
+import { useQuery } from "@tanstack/react-query";
+import { useFirebase } from "../../providers/firebase/useFirebase";
+import { getUser } from "./getUser";
+import { useAuth } from "../../providers/auth/useAuth";
+
+export const useGetUser = () => {
+  const { firestore } = useFirebase();
+  const { user: authUser } = useAuth();
+  return useQuery({
+    queryKey: ["user profile", authUser?.uid],
+    queryFn: () =>
+      getUser({
+        firestore,
+        userId: authUser?.uid ?? "",
+      }),
+    staleTime: 1000 * 60 * 5, // 5 minutes
+  });
+};
